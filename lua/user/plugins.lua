7a228e2ec96cb3b5f461f0006d5e03da0370982a
Barrett Poth <barrett@rnwbl.com>
2022-07-24 16:47:20 -0500

    initialize autopairs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -40,7 +40,8 @@ packer.init {
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
+
+  -- packer plugins
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
@@ -71,6 +72,9 @@ return packer.startup(function(use)
 
   -- Treesitter
   use "nvim-treesitter/nvim-treesitter"
+
+  -- other
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
