577de599e5e06c782c29db25d2ef2f88747dfdbc
Barrett Poth <barrett@rnwbl.com>
2022-07-24 21:00:50 -0500

    cleanup and format

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,10 +32,10 @@ keymap("n", "<Leader>c", ":Bdelete<CR>", opts)
 
 -- Telescope
 keymap(
-	"n",
-	"<leader>f",
-	"<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>",
-	opts
+  "n",
+  "<leader>f",
+  "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>",
+  opts
 )
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 
